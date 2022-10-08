c3c1dd5fda4628234af055fc175a6ea4dcff191c
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2022-10-08 03:49:04 +0200

    Add Matomo (#82)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,81 +1,87 @@
 module.exports = {
-  title: 'StarCraft2-API',
-  description: 'Flexible and feature-rich Node.js library for StarCraft II Community and Game Data APIs',
+  title: "StarCraft2-API",
+  description:
+    "Flexible and feature-rich Node.js library for StarCraft II Community and Game Data APIs",
   displayAllHeaders: true,
   head: [
-    ['link', { rel: 'icon', href: '/logo.png' }],
-    ['meta', { name: 'twitter:dnt', content: 'on' }],
+    ["link", { rel: "icon", href: "/logo.png" }],
+    ["meta", { name: "twitter:dnt", content: "on" }],
   ],
   plugins: {
     sitemap: {
-      hostname: 'https://starcraft2-api.lukem.net'
+      hostname: "https://starcraft2-api.lukem.net",
+    },
+    "@lukemnet/vuepress-plugin-matomo": {
+      siteId: 8,
+      trackerUrl: "https://s.lukem.eu/",
+      trackerJsFile: "vqmwssk.php",
     },
   },
   themeConfig: {
-    lastUpdated: 'Last Updated',
+    lastUpdated: "Last Updated",
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Docs', link: '/docs/' },
-      { text: 'GitHub', link: 'https://github.com/blizzapi/starcraft2-api' },
+      { text: "Home", link: "/" },
+      { text: "Docs", link: "/docs/" },
+      { text: "GitHub", link: "https://github.com/blizzapi/starcraft2-api" },
     ],
     sidebar: [
-      ['/', 'Home'],
+      ["/", "Home"],
       {
-        title: 'Getting started',
+        title: "Getting started",
         collapsable: true,
         children: [
-          '/docs/',
-          '/docs/requirements.md',
-          '/docs/install.md',
-          '/docs/examples.md',
+          "/docs/",
+          "/docs/requirements.md",
+          "/docs/install.md",
+          "/docs/examples.md",
         ],
       },
       {
-        title: 'Usage',
+        title: "Usage",
         collapsable: true,
         children: [
-          '/docs/usage/initialization.md',
-          '/docs/usage/custom-access-token.md',
-        ]
-      },
-      {
-        title: 'API methods',
-        collapsable: true,
-        children: [
-          '/docs/methods/',
-          '/docs/methods/queryStaticProfileData.md',
-          '/docs/methods/queryProfileMetadata.md',
-          '/docs/methods/queryProfile.md',
-          '/docs/methods/queryLadderSummary.md',
-          '/docs/methods/queryPlayerLadder.md',
-          '/docs/methods/queryGrandmasterLeaderboard.md',
-          '/docs/methods/queryLeagueData.md',
-          '/docs/methods/querySeason.md',
-          '/docs/methods/queryPlayerAccount.md',
-          '/docs/methods/queryLegacyProfile.md',
-          '/docs/methods/queryLegacyLadders.md',
-          '/docs/methods/queryLegacyMatchHistory.md',
-          '/docs/methods/queryLegacyLadder.md',
-          '/docs/methods/queryLegacyAchievements.md',
-          '/docs/methods/queryLegacyRewards.md',
-        ]
-      },
-      {
-        title: 'Utility methods',
-        collapsable: true,
-        children: [
-          '/docs/utils/',
-          '/docs/utils/getAllProfileUrlLocales.md',
-          '/docs/utils/checkIfProfileUrlLocaleLooksValid.md',
-          '/docs/utils/validateProfileUrlLocale.md',
-          '/docs/utils/checkIfProfileIdLooksValid.md',
-          '/docs/utils/validateProfileUrl.md',
-          '/docs/utils/unpackProfileUrl.md',
-          '/docs/utils/constructProfileUrl.md',
-          '/docs/utils/profileUrlRegex.md',
-          '/docs/utils/profileUrlLocaleRegex.md',
+          "/docs/usage/initialization.md",
+          "/docs/usage/custom-access-token.md",
         ],
       },
-    ]
-  }
-}
+      {
+        title: "API methods",
+        collapsable: true,
+        children: [
+          "/docs/methods/",
+          "/docs/methods/queryStaticProfileData.md",
+          "/docs/methods/queryProfileMetadata.md",
+          "/docs/methods/queryProfile.md",
+          "/docs/methods/queryLadderSummary.md",
+          "/docs/methods/queryPlayerLadder.md",
+          "/docs/methods/queryGrandmasterLeaderboard.md",
+          "/docs/methods/queryLeagueData.md",
+          "/docs/methods/querySeason.md",
+          "/docs/methods/queryPlayerAccount.md",
+          "/docs/methods/queryLegacyProfile.md",
+          "/docs/methods/queryLegacyLadders.md",
+          "/docs/methods/queryLegacyMatchHistory.md",
+          "/docs/methods/queryLegacyLadder.md",
+          "/docs/methods/queryLegacyAchievements.md",
+          "/docs/methods/queryLegacyRewards.md",
+        ],
+      },
+      {
+        title: "Utility methods",
+        collapsable: true,
+        children: [
+          "/docs/utils/",
+          "/docs/utils/getAllProfileUrlLocales.md",
+          "/docs/utils/checkIfProfileUrlLocaleLooksValid.md",
+          "/docs/utils/validateProfileUrlLocale.md",
+          "/docs/utils/checkIfProfileIdLooksValid.md",
+          "/docs/utils/validateProfileUrl.md",
+          "/docs/utils/unpackProfileUrl.md",
+          "/docs/utils/constructProfileUrl.md",
+          "/docs/utils/profileUrlRegex.md",
+          "/docs/utils/profileUrlLocaleRegex.md",
+        ],
+      },
+    ],
+  },
+};
